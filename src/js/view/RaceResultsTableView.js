b3a04ecaeb4d7b7d87cfc3f055d292086a3dc0cf
EnduranceCode <endurancecode@gmail.com>
2021-11-21 18:02:47 +0000

    Make results table sortable by column

--- src/js/view/RaceResultsTableView.js
+++ src/js/view/RaceResultsTableView.js
@@ -4,6 +4,8 @@
  * Licensed under MIT (https://github.com/EnduranceCode/endurancetrio-race-results/blob/master/LICENSE)
  */
 
+import naturalSort from '../vendor/naturalSort';
+
 class RaceResultsTableView {
   static buildResultsTable(resultsData) {
     const tableContainer = document.createElement('div');
@@ -30,17 +32,64 @@ class RaceResultsTableView {
     const thead = table.createTHead();
     const tr = thead.insertRow();
 
-    columnsLabels.forEach((label) => {
+    columnsLabels.forEach((label, index) => {
       const th = document.createElement('th');
-      th.classList.add('results-table__th', 'results-table__th--sort');
+
+      if (index == 0) {
+        th.classList.add('results-table__th', 'results-table__th--sort-ascending');
+      } else {
+        th.classList.add('results-table__th', 'results-table__th--sort');
+      }
+
       const textNode = document.createTextNode(label);
       th.appendChild(textNode);
       tr.appendChild(th);
     });
 
-    const rankColumn = tr.firstChild;
-    rankColumn.classList.remove('results-table__th--sort');
-    rankColumn.classList.add('results-table__th--sort-ascending');
+    RaceResultsTableView.enableRowSorting(table, tr.children);
+  }
+
+  static enableRowSorting(table, theadCells) {
+    for (let th of theadCells) {
+      th.onclick = () => {
+        const tableBody = table.tBodies[0];
+        const tableRows = tableBody.rows;
+
+        const sortDirection = th.classList.contains('results-table__th--sort-ascending') ? -1 : 1;
+        RaceResultsTableView.resetTheadCellsIcon(theadCells);
+
+        if (sortDirection == -1) {
+          th.classList.add('results-table__th--sort-descending');
+        } else {
+          th.classList.add('results-table__th--sort-ascending');
+        }
+
+        for (let tr of tableRows) {
+          Array.prototype.slice
+            .call(tableRows)
+            .sort((tr1, tr2) => {
+              const cellIndex = th.cellIndex;
+              naturalSort.insensitive = true;
+              return sortDirection * naturalSort(tr1.cells[cellIndex].textContent, tr2.cells[cellIndex].textContent);
+            })
+            .forEach((tr) => {
+              tableBody.appendChild(tableBody.removeChild(tr));
+            });
+        }
+      };
+    }
+  }
+
+  static resetTheadCellsIcon(theadCells) {
+    Array.from(theadCells).forEach((th) => {
+      if (
+        th.classList.contains('results-table__th--sort-ascending') ||
+        th.classList.contains('results-table__th--sort-descending')
+      ) {
+        th.classList.remove('results-table__th--sort-ascending', 'results-table__th--sort-descending');
+        th.classList.add('results-table__th--sort');
+      }
+    });
   }
 
   static appendResultsTableBody(table, resultsData) {
