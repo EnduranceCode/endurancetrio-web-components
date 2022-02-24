8165ead89c1309b9c71aa7a57230e448ef5e2007
Ricardo do Canto <endurancecode@gmail.com>
2022-02-24 11:06:06 +0000

    Prepare router introduction

--- src/app/RaceResults.js
+++ src/app/RaceResults.js
@@ -6,13 +6,7 @@
 
 import { LitElement, html, css } from 'lit';
 
-import { appStyles } from './css/app-style';
-import { Utils } from './utils/Utils';
-
-import { EventService } from './service/EventService';
-
-import './components/event-header/EventHeader';
-import './components/results-data/RaceData';
+import './components/events-list/EventsList';
 
 class RaceResults extends LitElement {
   static styles = [
@@ -25,47 +19,10 @@ class RaceResults extends LitElement {
         display: none;
       }
     `,
-    appStyles,
   ];
 
-  static properties = {
-    event: {},
-    eventReference: { attribute: 'event-reference' },
-    race: {},
-    raceReference: {},
-  };
-
-  constructor() {
-    super();
-    this.event = {};
-    this.eventReference = '';
-    this.race = {};
-    this.raceReference = '';
-  }
-
   render() {
-    return html`
-      <article>
-        <event-header .event="${this.event}"></event-header>
-
-        <race-data
-          location="${Utils.getComposedLocation(this.event.city, this.event.county, this.event.district)}"
-          .race="${this.race}"
-        ></race-data>
-      </article>
-    `;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    EventService.getEventByReference(this.eventReference).then((result) => {
-      this.event = result;
-    });
-
-    this.addEventListener('event-header-change-race', (e) => {
-      this.race = e.detail.race;
-      this.raceReference = e.detail.race.raceReference;
-    });
+    return html`<events-list event-reference="20220220FTP001"></events-list>`;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
   ],
 
   devServer: {
+    historyApiFallback: true,
     open: true,
   },
 };
