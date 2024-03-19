b121bf915ad1881ef562d9f84c31418df970e66b
Ricardo do Canto <endurancecode@gmail.com>
2024-03-19 20:49:35 +0000

    Add costum PDF file icons

---
+++ src/app/icons/mdi-endurancetrio.js
@@ -0,0 +1,10 @@
+/*!
+ * EnduranceTrio Web Components
+ * Copyright Ricardo do Canto
+ * Licensed under MIT (https://github.com/EnduranceCode/endurancetrio-race-results/blob/master/LICENSE)
+ */
+
+export const mdiEnduranceTrioFilePdf =
+  'M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6H6zm7 1.5L18.5 9H13V3.5zm-6.78 9.334h2.223c.711 0 1.336.62 1.336 1.332v.889c0 .71-.625 1.336-1.336 1.336h-.886v1.775H6.22v-5.332zm4.446 0h2.223c.71 0 1.332.62 1.332 1.332v2.668c0 .711-.621 1.332-1.332 1.332h-2.223v-5.332zm4.445 0h2.666v1.332h-1.332v.889h1.332v1.336h-1.332v1.775h-1.334v-5.332zm-7.554 1.332v.889h.886v-.889h-.886zm4.443 0v2.668h.889v-2.668H12z';
+export const mdiEnduranceTrioFilePdfOutline =
+  'M6 2a2 2 0 0 0-2 2v16c0 1.11.89 2 2 2h12c1.11 0 2-.89 2-2V8l-6-6H6zm0 2h7v5h5v11H6V4zm1 8.193v4.614h1.158V15.27h.766c.615 0 1.156-.541 1.156-1.156v-.77c0-.615-.54-1.152-1.156-1.152H7zm3.846 0v4.614h1.925a1.18 1.18 0 0 0 1.153-1.153v-2.308a1.18 1.18 0 0 0-1.153-1.153h-1.925zm3.847 0v4.614h1.155V15.27H17v-1.156h-1.152v-.77H17v-1.152h-2.307zm-6.535 1.153h.766v.77h-.766v-.77zm3.842 0h.771v2.308H12v-2.308z';
